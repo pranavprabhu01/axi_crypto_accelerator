d1e9a9af9aa75e56674cfead08151b3436d792e7
Pranav Prabhu <prnv.prbh@gmail.com>
2026-05-26 04:22:56 +0200

    Added description for crypto RTL file

---
+++ rtl/crypto_core.sv
@@ -0,0 +1,8 @@
+//Description: This module performs encryption of the input text using AES
+//128.
+//Author: Pranav Prabhu
+//Email:pp7437@rit.edu;prnv.prbh@gmail.com
+//Input: Plaintext
+//Output: Ciphertext
+//Total Rounds: 10
+//Encryption Rounds:9 Last Round:1
