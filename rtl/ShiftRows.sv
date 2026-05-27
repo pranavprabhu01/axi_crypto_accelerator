176ce4bf8af0e21841415d495491929fb3b2181b
Pranav Prabhu <prnv.prbh@gmail.com>
2026-05-27 21:45:28 +0200

    Added ShiftRows module

---
+++ rtl/ShiftRows.sv
@@ -0,0 +1,32 @@
+//Description:This module takes a 4x4 matrix where each element is one byte
+//and shifts row by one.
+//Author:Pranav Prabhu
+//Email:pp7437@rit.edu;prnv.prbh@gmail.com
+
+
+module ShiftRows(input logic [7:0]i_matrix[0:3][0:3],output logic[7:0] o_shifted [0:3][0:3])
+
+//Assigning First Row for shift
+	assign o_shifted[0][0] = i_matrix[0][0];
+        assign o_shifted[0][1] = i_matrix[0][1];
+	assign o_shifted[0][2] = i_matrix[0][2];
+	assign o_shifted[0][3] = i_matrix[0][3];
+
+//Assigning Second Row for shift
+	assign o_shifted[1][0] = i_matrix[1][1];
+	assign o_shifted[1][1] = i_matrix[1][2];
+	assign o_shifted[1][2] = i_matrix[1][3];
+	assign o_shifted[1][3] = i_matrix[1][0];
+
+//Assigning Third Row for shift
+	assign o_shifted[2][0] = i_matrix[2][1];
+	assign o_shifted[2][1] = i_matrix[2][2];
+	assign o_shifted[2][2] = i_matrix[2][3];
+	assign o_shifted[2][3] = i_matrix[2][0];
+
+//Assigning Fourth Row for shift
+	assign o_shifted[3][0] = i_matrix[3][1];
+	assign o_shifted[3][1] = i_matrix[3][2];
+	assign o_shifted[3][2] = i_matrix[3][3];
+	assign o_shifted[3][3] = i_matrix[3][0];
+endmodule
