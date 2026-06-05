590aff6d21d13aae5ab5e6b7e117f371313c3103
Pranav Prabhu <prnv.prbh@gmail.com>
2026-06-05 04:35:09 +0200

    Adding AddRoundKey Module with port list

---
+++ rtl/AddRoundKey.sv
@@ -0,0 +1,11 @@
+//Description:This module performs bitwise XOR with round key bytes.
+//Author:Pranav Prabhu
+//Email:pp7437@rit.edu;prnv.prbh@gmail.com
+
+
+`default_nettype none
+
+
+module AddRoundKey(input logic [127:0] i_data,output logic [127:0] o_data)
+
+endmodule
