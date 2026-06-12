7f0729832daf0cb3159f57a45b0b82fd3df1d50a
Pranav Prabhu <prnv.prbh@gmail.com>
2026-06-12 21:21:11 +0200

    Constant matrix LUT declared

--- rtl/MixColumns.sv
+++ rtl/MixColumns.sv
@@ -3,5 +3,14 @@
 //Email:pp7437@rit.edu;prnv.prbh@gmail.com
 
 module MixColumns(input logic [127:0]i_data,output logic [127:0]o_data);
-	
+
+	//LUT of constant matrix
+	reg [8:0]aes_matrix[3:0][3:0] = '{
+		//Column:0	1	2	3
+		'{8'h02,8'h03,8'h01,8'h01},	//Row 0
+		'{8'h01,8'h02,8'h03,8'h01},	//Row 1
+		'{8'h01,8'h01,8'h02,8'h03},	//Row 2
+		'{8'h03,8'h01,8'h01,8'h02}	//Row 3
+		};
+
 endmodule
