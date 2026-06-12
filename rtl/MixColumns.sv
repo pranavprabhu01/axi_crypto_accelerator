e69e52299f4d46c27be7950c81fc7f97cc8c45e3
Pranav Prabhu <prnv.prbh@gmail.com>
2026-06-12 19:05:14 +0200

    MixColumns with IO ports

---
+++ rtl/MixColumns.sv
@@ -0,0 +1,7 @@
+//Description: This module multiplies state matrix with a constant matrix.
+//Author: Pranav Prabhu
+//Email:pp7437@rit.edu;prnv.prbh@gmail.com
+
+module MixColumns(input logic [127:0]i_data,output logic [127:0]o_data);
+	
+endmodule
