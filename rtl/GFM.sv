d5bc53615b1da96bef46a4b75d1b28c2f43bebcb
Pranav Prabhu <prnv.prbh@gmail.com>
2026-06-19 22:49:21 +0200

    Adding logic in GFM Module

--- rtl/GFM.sv
+++ rtl/GFM.sv
@@ -4,5 +4,17 @@ Email: pp7437@rit.edu;prnv.prbh@gmail.com
 
 module GFM (input [7:0] i_data_a,input [7:0] i_data_b, output [7:0] o_data);
 
-	
+	//Intermediate Data
+	logic [7:0] intr_data[0:7];
+
+	//Iteration Variable
+	int i;
+
+	//Logic: loop through i_data_b; check bit;if its set left shift	
+	for(i = 7;i > -1;i = i-1) begin
+		if(i_data_b[i] == 1'b1)begin
+		end
+		else begin
+		end
+	end
 endmodule
