e5425b565cd7ccb8dc4f5ece255d7859bb2c6d54
Pranav Prabhu <prnv.prbh@gmail.com>
2026-06-19 03:57:34 +0200

    Added Galios Field Multiplication with IO ports

---
+++ rtl/GFM.sv
@@ -0,0 +1,8 @@
+Description: This module performs Galois Field Multiplication.
+Author: Pranav Prabhu
+Email: pp7437@rit.edu;prnv.prbh@gmail.com
+
+module GFM (input [7:0] i_data_a,input [7:0] i_data_b, output [7:0] o_data);
+
+	
+endmodule
