396695d0ad3ce32c718e13ebfe9127487cee9ccc
Pranav Prabhu <prnv.prbh@gmail.com>
2026-06-21 22:41:42 +0200

    GFM logic added. Synth fails at line 28 continue. Need to check.

--- rtl/GFM.sv
+++ rtl/GFM.sv
@@ -1,20 +1,34 @@
-Description: This module performs Galois Field Multiplication.
-Author: Pranav Prabhu
-Email: pp7437@rit.edu;prnv.prbh@gmail.com
+//Description: This module performs Galois Field Multiplication.
+//Author: Pranav Prabhu
+//Email: pp7437@rit.edu;prnv.prbh@gmail.com
 
 module GFM (input [7:0] i_data_a,input [7:0] i_data_b, output [7:0] o_data);
 
 	//Intermediate Data
-	logic [7:0] intr_data[0:7];
+	reg [7:0] intr_data;
 
 	//Iteration Variable
 	int i;
 
+	always_comb begin
+	
+	 intr_data = i_data_a;
 	//Logic: loop through i_data_b; check bit;if its set left shift	
-	for(i = 7;i > -1;i = i-1) begin
+	for(i = 0;i < 8;i = i+1) begin
 		if(i_data_b[i] == 1'b1)begin
+			if(i_data_a[7]==1'b1) begin
+				intr_data = (intr_data<<1)^(8'h1B); 
+			end
+			else begin
+				intr_data = (intr_data<<1);
+			end
 		end
 		else begin
+			//Do Nothing
+			continue;
 		end
 	end
+end
+
+	assign o_data = intr_data;
 endmodule
