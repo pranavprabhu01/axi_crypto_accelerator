68ed3d93016edc989f2336637a5688fabe9a3b18
Pranav Prabhu <prnv.prbh@gmail.com>
2026-07-17 21:05:58 +0200

    Defining GFM function

--- tb/test_GFM.py
+++ tb/test_GFM.py
@@ -4,6 +4,26 @@
 
 import cocotb
 from cocotb.triggers import Timer
+import random
+
+def gfm_reference(a,b):
+
+    acc = 0
+    intr_data = a &0xFF
+
+    for i in range(8):
+        if(b>>i)&1:
+            acc^=intr_data
+
+        if intr_data & 0x80:
+            intr_data = ((intr_data<<1)^0x1B)&0xFF
+        else:
+            intr_data = (intr_data<<1)&0xFF
+    return acc & 0xFF
 
 @cocotb.test()
 async def GFM_test(dut):
+    #test vector
+    test_vector_a =""
+    test_vector_b =""
+
