342f78fd8f57a7e8b700101b2fbb43062a28e654
Pranav Prabhu <prnv.prbh@gmail.com>
2026-08-02 23:02:55 +0200

    defining tb for mixcolumns

--- tb/test_MixColumns.py
+++ tb/test_MixColumns.py
@@ -4,6 +4,36 @@
 
 import cocotb
 from cocotb.triggers import Timer
+import random
 
+def gfm_reference(a,b):
+
+    acc = 0
+    intr_data = a &0xFF
+
+    for i in range(8):
+        if(b>>i)&1:
+            acc^=intr_data
+
+        if intr_data & 0x80:
+            intr_data = ((intr_data<<1)^0x1B)&0xFF
+        else:
+            intr_data = (intr_data<<1)&0xFF
+    return acc & 0xFF
+
+
+#AES Matrix
+AES_MATRIX = [
+	[0x02,0x03,0x01,0x01],
+	[0x01,0x02,0x03,0x01],
+	[0x01,0x01,0x02,0x03],
+	[0x03,0x01,0x01,0x02]
+	]
+
+def mix_columns_reference(input_128bit):
+	"""
+	computing mixcolumn transformation matrix
+	"""
+	state =[]
 @cocotb.test()
 async def mixcolumns_test(dut):
